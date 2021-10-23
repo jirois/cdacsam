a747288710fd0ad49971b593ed2b55a4f3d6d31e
Lin <omasajiri@gmail.com>
2021-10-23 01:45:36 +0100

    feat: added add_client input page

--- app/src/main/java/com/jinncyapps/authenapp/activities/AddClientActivity.kt
+++ app/src/main/java/com/jinncyapps/authenapp/activities/AddClientActivity.kt
@@ -1,12 +1,41 @@
 package com.jinncyapps.authenapp.activities
 
-import android.os.Bundle
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import com.jinncyapps.authenapp.R
+import com.jinncyapps.authenapp.databinding.ActivityAddClientBinding
 
-class AddClientActivity: AppCompatActivity() {
+class AddClientActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddClientBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_client)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_add_client)
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.purple_700)
+        }
+
+        setupActionBar()
+
+    }
+
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarAddclientActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        }
+        binding.tvTitle.text = resources.getString(R.string.tv_add_client_title)
+
+        binding.toolbarAddclientActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
