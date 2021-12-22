c1f018a3a9ef7e9cb7119a4c5e97fd6431e36e89
Omas Ajiri <omasajiri@gmail.com>
2021-12-22 20:53:58 +0100

    fix: permission dependency

--- app/src/main/java/com/jinncyapps/authenapp/utils/Constants.kt
+++ app/src/main/java/com/jinncyapps/authenapp/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.jinncyapps.authenapp.utils
 
 object Constants {
     const val USERS:String = "users"
+    const val PERMISSION_LOCATION_REQUEST_CODE = 0
 }
---
+++ app/src/main/java/com/jinncyapps/authenapp/utils/Permissions.kt
@@ -0,0 +1,24 @@
+package com.jinncyapps.authenapp.utils
+
+import android.Manifest
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.jinncyapps.authenapp.utils.Constants.PERMISSION_LOCATION_REQUEST_CODE
+import com.vmadalin.easypermissions.EasyPermissions
+
+object Permissions {
+    fun hasLocationPermission(context: Context) =
+        EasyPermissions.hasPermissions(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+    fun requestLocationPermission(fragment: Fragment){
+        EasyPermissions.requestPermissions(
+            fragment,
+            "This application can not work without location permission",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+}
