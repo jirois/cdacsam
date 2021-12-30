e2ff2cd09e18f8869af8b2243afff4fbefb59a1c
Omas Ajiri <omasajiri@gmail.com>
2021-12-30 21:55:48 +0100

    fix: update dependencies

---
+++ app/src/main/java/com/jinncyapps/cdacsam/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.jinncyapps.cdacsam.splash
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.jinncyapps.cdacsam.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.purple_700)
+
+        Handler().postDelayed({
+
+            finish()
+        }, 2500)
+    }
+}
